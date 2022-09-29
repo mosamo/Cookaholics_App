617e4435f136266d49dc0ff4edf56197e142147e
mosamo <please.reply.to.me.or.else@gmail.com>
2022-09-29 20:26:25 +0400

    Portal Activity pre-merge

--- app/src/main/java/com/mohamed_mosabeh/cookaholics_capstone/PortalActivity.java
+++ app/src/main/java/com/mohamed_mosabeh/cookaholics_capstone/PortalActivity.java
@@ -1,19 +1,22 @@
 package com.mohamed_mosabeh.cookaholics_capstone;
 
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
 import com.firebase.ui.auth.IdpResponse;
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -25,8 +28,9 @@ public class PortalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_portal);
-
+    
         mAuth = FirebaseAuth.getInstance();
+        mAuth.signOut();
 
         mAuthStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
