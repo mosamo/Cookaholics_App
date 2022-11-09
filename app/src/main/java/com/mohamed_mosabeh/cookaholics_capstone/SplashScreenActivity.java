6c1bf3118b8ead6bf57cb9c4c26dbafff9cfa21f
mosamo <please.reply.to.me.or.else@gmail.com>
2022-11-10 02:02:09 +0400

    Wired Up SplashScreenActivity.java and removed proprietorial images

--- app/src/main/java/com/mohamed_mosabeh/cookaholics_capstone/SplashScreenActivity.java
+++ app/src/main/java/com/mohamed_mosabeh/cookaholics_capstone/SplashScreenActivity.java
@@ -1,14 +1,31 @@
 package com.mohamed_mosabeh.cookaholics_capstone;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.mohamed_mosabeh.utils.ViewUtils;
+
 public class SplashScreenActivity extends AppCompatActivity {
+    
+    private Button mMainButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+        
+        mMainButton = findViewById(R.id.ssSplashStartButton);
+        mMainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SplashScreenActivity.this, PortalActivity.class));
+                finish();
+            }
+        });
+        
     }
 }
